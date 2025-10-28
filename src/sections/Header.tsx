8c2f71768137f5ca0c014dd7000a9d748b284124
Ayman Elfeky <aywork73@gmail.com>
2025-10-29 00:42:57 +0300

    Complete Changes

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,6 @@
 import { MenuIcon } from "@/assets";
 import Logoo  from "../assets/logo.png";
+import Log from "../assets/LOGO-Opt.jpg";
 import { Button } from "@/components/Button";
 import Link from "next/link";
 import Image from "next/image";
@@ -13,18 +14,19 @@ const navItems = [
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 py-4 border-b border-white/15 md:border-none z-10">
-      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
-      <div className="container">
-        <div className="relative flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
-          <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
-          <div>
-            <div className="inline-flex items-center justify-center size-10 border border-white/15 rounded-lg">
-              <Image src={Logoo} alt="Logo" className="w-10 h-10 object-contain" />
-            </div>
+    <header className="sticky top-0 py-4 md:py-6 border-none z-10">
+      {/* <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div> */}
+      <div className="">
+        <div className="relative flex justify-between items-center md:p-2.5 rounded-xl max-w-6xl mx-auto">
+          {/* <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div> */}
+          <div className="flex items-center gap-2">
+            {/* <div className="size-20 border border-white/15 rounded-lg"> */}
+              <Image src={Logoo} alt="Logo" className="w-24 h-16 object-contain" />
+              <h1 className="text-2xl font-bold md:hidden">Premium Auto Spa</h1>
+            {/* </div> */}
           </div>
           <div className="hidden md:block">
-            <nav className="flex gap-8 text-sm">
+            <nav className="flex gap-8 text-sm border border-white/15 py-4 px-4 rounded-lg bg-white/5 backdrop-blur">
               {navItems.map((link, index) => (
                 <Link
                   key={index}
